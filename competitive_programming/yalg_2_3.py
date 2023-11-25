cd92edfb2dd03319824a457bc037582b050132cf
gonzoden <deniz.nazarova.99@gmail.com>
2023-11-25 21:35:55 +0600

    14/31

---
+++ competitive_programming/yalg_2_3.py
@@ -0,0 +1,24 @@
+s = input().strip()
+
+
+def putin_loh(s):
+    n = len(s)
+    z = [0] * n
+    l, r = 0, 0
+
+    for i in range(1, n):
+        if i <= r:
+            z[i] = min(r - i + 1, z[i - l])
+
+        while i + z[i] < n and s[z[i]] == s[i + z[i]]:
+            z[i] += 1
+
+        if i + z[i] - 1 > r:
+            l, r = i, i + z[i] - 1
+
+    return z
+
+z_array = putin_loh(s)
+
+print(" ".join(map(str, z_array)))
+
